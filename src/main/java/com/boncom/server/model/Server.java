f4134bbae39897dacf433edcbd93bec0bc22fd4b
Nabza0 <victorcubamalatsi@gmail.com>
2023-03-13 15:33:21 -1100

    my first commit

---
+++ src/main/java/com/boncom/server/model/Server.java
@@ -0,0 +1,11 @@
+package com.boncom.server.model;
+
+public class Server {
+    private Long id;
+    private String ipAddress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
